19bd4ca291c6374008f68d7b4af91cde30389992
sebiisgarb <seby3pop.ps10@gmail.com>
2025-07-31 14:31:47 +0300

    login + users

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.db import models
+import re
 
+
+PHONE_REGEX = re.compile(r"^0[7-9]\d{8}$")
 
 class Roles(models.TextChoices):
     SITE_MANAGER = 'SITE_MANAGER', 'Site Manager'
@@ -8,19 +11,18 @@ class Roles(models.TextChoices):
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra):
-        if not email:
-            raise ValueError("Email is required")
+    def create_user(self, phone_number: str, password=None, **extra):
+        if not phone_number or not PHONE_REGEX.match(phone_number):
+            raise ValueError("Nr de telefon gresit")
         if password is None:
             raise ValueError("Password is required")
 
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra)
+        user = self.model(phone_number=phone_number, **extra)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password, **extra):
+    def create_superuser(self, phone_number: str, password: str, **extra):
         extra.setdefault('is_staff', True)
         extra.setdefault('is_superuser', True)
 
@@ -29,19 +31,19 @@ class UserManager(BaseUserManager):
         if password is None:
             raise ValueError("Superusers must have a password")
 
-        return self.create_user(email, password, **extra)
+        return self.create_user(phone_number, password, **extra)
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, unique=True, default="0712345678")
     first_name = models.CharField(max_length=50, blank=True)
     last_name = models.CharField(max_length=50, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     role = models.CharField(max_length=15, choices=Roles.choices, default=Roles.DRIVER)
 
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS: list[str] = []
 
     objects = UserManager()
 
@@ -50,4 +52,4 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         verbose_name_plural = "users"
 
     def __str__(self):
-        return self.email
+        return self.phone_number
